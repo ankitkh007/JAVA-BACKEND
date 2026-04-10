2f3032502a1628b09c06c68e35f59d09778fd20d
ankitkh007 <ankit123khanna456@gmail.com>
2026-04-11 00:19:59 +0530

    Replaced Concurrent HashMap with built-in JPA Repository

--- student-management-api/src/main/java/com/ankit/student_management_api/repository/StudentRepository.java
+++ student-management-api/src/main/java/com/ankit/student_management_api/repository/StudentRepository.java
@@ -1,27 +1,30 @@
 package com.ankit.student_management_api.repository;
 
 import com.ankit.student_management_api.model.Student;
-import java.util.*;
-import java.util.concurrent.ConcurrentHashMap;
+//import java.util.*;
+//import java.util.concurrent.ConcurrentHashMap;
+
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class StudentRepository {
-    private final Map<Integer, Student> students = new ConcurrentHashMap<>();
+public interface StudentRepository extends JpaRepository<Student, Integer> {
 
-    public Collection<Student> findAll() {
-        return students.values();
-    }
-
-    public Student findById(Integer id) {
-        return students.get(id);
-    }
-
-    public boolean save(Student student) {
-        return students.putIfAbsent(student.getId(), student) == null;
-    }
-
-    public Student delete(Integer id) {
-        return students.remove(id);
-    }
 }
+// private final Map<Integer, Student> students = new ConcurrentHashMap<>();
+
+// public Collection<Student> findAll() {
+// return students.values();
+// }
+
+// public Student findById(Integer id) {
+// return students.get(id);
+// }
+
+// public boolean save(Student student) {
+// return students.putIfAbsent(student.getId(), student) == null;
+// }
+
+// public Student delete(Integer id) {
+// return students.remove(id);
+// }
