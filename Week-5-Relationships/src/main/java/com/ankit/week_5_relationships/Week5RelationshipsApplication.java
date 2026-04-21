bc12f135e2f8b2b185a3fd9b7b43d981e062613a
ankitkh007 <ankit123khanna456@gmail.com>
2026-04-21 08:45:02 +0530

    Week-5 Relationships.

---
+++ Week-5-Relationships/src/main/java/com/ankit/week_5_relationships/Week5RelationshipsApplication.java
@@ -0,0 +1,13 @@
+package com.ankit.week_5_relationships;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week5RelationshipsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Week5RelationshipsApplication.class, args);
+	}
+
+}
---
+++ Week-5-Relationships/src/test/java/com/ankit/week_5_relationships/Week5RelationshipsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ankit.week_5_relationships;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week5RelationshipsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
